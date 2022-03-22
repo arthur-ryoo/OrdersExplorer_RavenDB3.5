a78caae395854b44065718d7fdb61afeac489990
Arthur Ryoo <jene0523@gmail.com>
2022-03-22 10:21:02 -0700

    Add project files.

---
+++ DocumentStoreHolder.cs
@@ -0,0 +1,23 @@
+﻿using Raven.Client;
+using Raven.Client.Document;
+using System;
+
+namespace OrdersExplorer
+{
+    public static class DocumentStoreHolder
+    {
+        private static readonly Lazy<IDocumentStore> LazyStore =
+            new Lazy<IDocumentStore>(() =>
+            {
+                var store = new DocumentStore
+                {
+                    ConnectionStringName="RavenDB"
+                };
+
+                return store.Initialize();
+            });
+
+        public static IDocumentStore Store =>
+            LazyStore.Value;
+    }
+}
